0ae70b59472045f085783a99e3f34b5b0214bce6
Raaho <emailkaaga@example.com>
2026-06-05 21:42:57 -0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,22 @@
-import React from "react";
-import { StrictMode } from "react";
-import ReactDOM from "react-dom/client";
-import Farta from "./Farta.jsx";
+// import React from "react";
+// import { StrictMode } from "react";
+// import ReactDOM from "react-dom/client";
+// // import Farta from "./Farta.jsx";
 // import Student from "./Student.jsx";
-// import ProductPegination from "./ProductPegination.jsx";
-ReactDOM.createRoot(document.getElementById("root")).render( 
-<StrictMode>
-    <Farta/>
-  </StrictMode>
-  );
+// // import Student from "./Student.jsx";
+// // import ProductPegination from "./ProductPegination.jsx";
+// ReactDOM.createRoot(document.getElementById("root")).render( 
+// <StrictMode>
+//     <Student/>
+//   </StrictMode>
+//   );
 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Student from "./Student.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Student />
+  </React.StrictMode>
+);
